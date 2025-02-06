9632fe5f5bacd677e5275b7bbc6b5edbd8c065f9
mazzjailer <mzyramini@gmail.com>
2025-02-06 17:45:12 +0330

    add error, loading, and not found pages

---
+++ src/app/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <>
+      <div className='flex flex-row rounded-2xl items-center justify-center h-[84vh] w-screen' ><h4 className='text-black text-2xl'>An error occurred while loading the page.</h4></div>
+    </>
+  )
+}
+
+export default ErrorPage
---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+      <div className='flex flex-row rounded-2xl items-center justify-center h-[84vh] w-screen' ><h4 className='text-black text-2xl'>The requested page does not exist!</h4></div>
+    </>
+  )
+}
+
+export default NotFound
