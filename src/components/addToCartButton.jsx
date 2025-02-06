2bcbe3baf9ec85ef3110158c5cae96507490507f
mazzjailer <mzyramini@gmail.com>
2025-02-06 05:10:11 +0330

    implement addToCart and addToFavourites buttons

--- src/components/addToCartButton.jsx
+++ src/components/addToCartButton.jsx
@@ -1,10 +1,32 @@
 'use client'
 import React from 'react'
 import { Button } from './ui/button'
+import { useCart } from "../contexts/cartContext.jsx"
 
-const AddToCartButton = () => {
+const AddToCartButton = (props) => {
+  const {addToCart, cartItems, updateCartItemQuantity, removeItem} = useCart();
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  }
+  const handleUpdateCartItemQuantity = (product, quantity) => {
+    if (quantity === 1) {
+      removeItem(product);
+    }
+    else {
+      updateCartItemQuantity(product, quantity - 1);
+    }
+  }
+  
   return (
-    <Button className='font-bold w-full'>ADD TO CART</Button>
+    <div>
+      { cartItems.find(item => item.id === props.product.id) ? <div className='flex flex-row items-center justify-start gap-1'>
+        <Button onClick={() => handleUpdateCartItemQuantity(props.product, cartItems.find(item => item.id === props.product.id).quantity)} className='font-bold rounded-xl'>-</Button>
+        <Button className='font-bold rounded-xl bg-white text-black cursor-default hover:bg-white shadow-none border border-gray-200'>
+          {cartItems.find(item => item.id === props.product.id).quantity}
+        </Button>
+        <Button onClick={() => handleAddToCart(props.product)} className='font-bold rounded-xl'>+</Button>
+      </div> : <Button onClick={() => handleAddToCart(props.product)} className='font-bold w-full rounded-xl'>ADD TO CART</Button>}
+    </div>
   )
 }
 
--- src/components/addToFavouritesButton.jsx
+++ src/components/addToFavouritesButton.jsx
@@ -1,23 +1,37 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { Button } from "./ui/button";
 import { FaHeart } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa6";
+import { useFav } from '@/contexts/favContext';
 
-const AddToFavouritesButton = () => {
-  const [isClicked, setIsClicked] = useState(false)
+const AddToFavouritesButton = (props) => {
+  const [isFav, setIsFav] = useState(false)
+  const {addToFav, removeItem, favItems} = useFav();
 
-  const handleClick = () => {
-    setIsClicked(!isClicked)
+  useEffect(() => {
+    if (!props.product) return;
+    const isProductFav = favItems.some(item => item.id === props.product.id);
+    setIsFav(isProductFav);
+  }, [favItems, props.product])
+
+  const handleClick = (product) => {
+    if (isFav) {
+      removeItem(product);
+    } else {
+      addToFav(product);
+    }
+    setIsFav(!isFav);
   }
+  
   return (
     <Button
       size='icon' 
-      onClick={handleClick} 
+      onClick={() => handleClick(props.product)} 
       className='transition-all duration-300 rounded-xl bg-white border-2 border-black hover:bg-gray-100'
     >
-      {isClicked ? <FaHeart className='text-[#5A3D25]' /> : <FaRegHeart className='text-[#5A3D25]' />}
+      {isFav ? <FaHeart className='text-[#5A3D25]' /> : <FaRegHeart className='text-[#5A3D25]' />}
     </Button>
   )
 }
