9bc1cc6189b14f2aba84864bcfe2911a7d4b6d94
mazzjailer <mzyramini@gmail.com>
2025-02-06 23:06:48 +0330

    refactor loading and footer components for improved styling

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import { FiLoader } from "react-icons/fi";
 const Loading = () => {
   return (
     <div className='flex h-screen items-center justify-center'>
-      <div className='flex flex-row bg-[#5A3D25] shadow-inner p-6 rounded-2xl items-center justify-center z-50' >
+      <div className='flex flex-row bg-[#5A3D25] shadow-inner p-5 rounded-2xl items-center justify-center z-50' >
         <FiLoader className='text-white text-xl animate-spin mr-2' /><h4 className='text-white text-xl font-medium'>Loading...</h4>
       </div>
     </div>
