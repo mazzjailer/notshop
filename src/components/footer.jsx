b0e852f75bed25b580af6ebb70d8056b10f84517
mazzjailer <mzyramini@gmail.com>
2025-02-10 01:19:18 +0330

    fix footer layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import { FaHeart } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-      <div className='flex flex-row justify-center items-center w-full h-20 md:h-32 text-[#5A3D25] font-bold text-md md:text-lg'>
+      <div className='flex flex-row justify-center items-center w-full h-20 md:h-32 text-[#5A3D25] font-bold text-md md:text-lg mt-auto'>
       Made with&nbsp;<FaHeart className='text-red-500 mx-1' />&nbsp;by&nbsp;<Link className='hover:underline font-medium' href='https://github.com/mazzjailer'>Maziyar Amini</Link>
     </div>
   )
