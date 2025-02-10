547dc70f5d8fadf7b88ebc2f5d47e60efcd03322
mazzjailer <mzyramini@gmail.com>
2025-02-10 20:16:53 +0330

    adjust navigation menu viewport positioning for improved layout on mobile devices

--- src/components/ui/navigation-menu.jsx
+++ src/components/ui/navigation-menu.jsx
@@ -60,7 +60,7 @@ NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName
 const NavigationMenuLink = NavigationMenuPrimitive.Link
 
 const NavigationMenuViewport = React.forwardRef(({ className, ...props }, ref) => (
-  <div className={cn("absolute left-0 top-full flex justify-center")}>
+  <div className={cn("absolute -left-16 md:left-0 top-full flex justify-center")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
         "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-3xl border bg-popover text-popover-foreground shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",
